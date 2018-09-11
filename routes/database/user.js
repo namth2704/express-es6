f262abae00479770f81894a2945ce8b2db3f33a3
namth2704 <namth27494@outlook.com>
2018-09-11 17:16:41 +0700

    complete database interaction template

--- routes/database/user.js
+++ routes/database/user.js
@@ -7,36 +7,58 @@ router.get('/get/:id', async (req, res) => {
     const params = req.params;
     const id = params ? params.id : null;
 
-    if (id) {
-        try {
-            const user = await UserFunctions.GetUserByID(id);
-            res.json(user);
-        }
-        catch (ex) {
-            res.send(`Error while getting user by id=${id}: ${ex}`);
-        }
+    try {
+        res.json(await UserFunctions.GetUserByID(id));
     }
-    else {
-        res.send('Required userID');
+    catch (ex) {
+        res.send(`Error while getting user by id=${id}: ${ex}`);
     }
 });
 
 router.post('/add', async (req, res) => {
     try {
-        const username = req.body.name;
-        const birthday = req.body.birthday;
-        if (username && birthday) {
-            const user = await UserFunctions.AddUser(username, birthday);
-            if (user) {
-                res.send(`Add user success: ${user}`);
-            }
-            else {
-                res.send(`Error while adding user`);
-            }
+        const userData = req.body;
+        if (userData) {
+            const result = await UserFunctions.AddUser(userData);
+            res.send(`Add user successfully: ${JSON.stringify(result)}`);
         }
     }
     catch (ex) {
-        console.log(ex);
+        res.send(`Error while adding user: ${ex}`);
+    }
+});
+
+router.post('/update', async (req, res) => {
+    try {
+        const userid = req.body.userid;
+
+        if (userid) {
+            const updatedUserData = req.body;
+            const result = await UserFunctions.UpdateUser(userid, updatedUserData);
+            res.send(`Update user successfully: ${JSON.stringify(result)}`);
+        }
+        else {
+            res.send('Please provide userid to update the user information');
+        }
+    }
+    catch (ex) {
+        res.send(`Error while updating user: ${ex}`);
+    }
+});
+
+router.post('/delete', async (req, res) => {
+    try {
+        const userid = req.body.userid;
+
+        if (userid) {
+            const result = await UserFunctions.DeleteUser(userid);
+            res.send(`Delete user successfully: ${JSON.stringify(result)}`);
+        }
+        else {
+            res.send('Please provide userid to delete the user information');
+        }
+    }
+    catch (ex) {
         res.send(`Error while adding user: ${ex}`);
     }
 });
